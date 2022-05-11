12909ce2b4ae46733002265c9cea4e2851d47d04
SiemenSlabbinck <siemen.slabbinck@student.ap.be>
2022-05-12 01:14:43 +0200

    Add different type questions

--- lib/student/questions.dart
+++ lib/student/questions.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/admin/examens/codecorrectie/correctievraag.class.dart';
 import 'package:flutter_project/admin/studenten/addmultiplestudent.dart';
 import 'package:flutter_project/admin/studenten/addstudent.dart';
 import 'package:flutter_project/admin/studenten/student.class.dart';
 import 'package:flutter_project/student/question.class.dart';
 import 'package:geolocator/geolocator.dart';
 import '../../services/toaster.dart';
+import '../styles/styles.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({Key? key}) : super(key: key);
@@ -21,6 +23,17 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       .map((snapshot) =>
           snapshot.docs.map((doc) => Question.fromJson(doc.data())).toList());
 
+  var _questionIndex = 0;
+  late List<Question> questions = [];
+
+  void _answerQuestion() {
+    setState(() {
+      if (_questionIndex != (questions.length - 1)) {
+        _questionIndex = _questionIndex + 1;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -30,18 +43,92 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             stream: readQuestions(),
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                final questions = snapshot.data!;
-                return ListView(
-                  children: questions.map(buildQuestion).toList(),
-                );
+                questions = snapshot.data!;
+                if (questions[_questionIndex].type == "open") {
+                  return openQuestion(questions, _questionIndex);
+                } else if (questions[_questionIndex].type == "correctie") {
+                  return correctionQuestion(questions, _questionIndex);
+                } else {
+                  return multipleChoiceQuestion(questions, _questionIndex);
+                }
               } else {
-                print(snapshot);
                 return const Center(child: CircularProgressIndicator());
               }
             }),
       );
 
-  Widget buildQuestion(Question question) => ListTile(
-        title: Text(question.vraag),
-      );
+  Widget openQuestion(List<Question> questions, int index) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            questions[index].vraag,
+            style: Styles.headerStyleH1,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Antwoord',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _answerQuestion();
+            },
+            child: const Text('Next'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget correctionQuestion(List<Question> questions, int index) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            questions[index].vraag,
+            style: Styles.headerStyleH1,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Correctie',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _answerQuestion();
+            },
+            child: const Text('Next'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget multipleChoiceQuestion(List<Question> questions, int index) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            questions[index].vraag,
+            style: Styles.headerStyleH1,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'multiple',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _answerQuestion();
+            },
+            child: const Text('Next'),
+          )
+        ],
+      ),
+    );
+  }
 }
